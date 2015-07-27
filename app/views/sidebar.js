7e483bd9742f5a323872402e8772c23fb733cf4c
Abdessamad <a.mouhassine@gmail.com>
2015-07-27 20:18:13 +0100

    add topbar and sidebar views and templates

--- app/views/sidebar.js
+++ app/views/sidebar.js
@@ -25,28 +25,11 @@
 define(function(require, exports, module) {
     'use strict';
     
-    require('bootstrap');
-    
     var Marionette = require('backbone.marionette');
-    var TopbarView = require('./topbar');
-    var SidebarView = require('./sidebar');
     
-    var LayoutView = Marionette.LayoutView.extend({
-        el: 'body',
-        
-        template: require('template!../templates/layout'),
-        
-        regions: {
-            header: 'header',
-            sidebar: 'aside',
-            content: 'main'
-        },
-        
-        onRender: function() {
-            this.header.show(new TopbarView());
-            this.sidebar.show(new SidebarView());
-        }
+    var View = Marionette.ItemView.extend({
+        template: require('template!../templates/sidebar')
     });
     
-    module.exports = LayoutView;
+    module.exports = View;
 });
