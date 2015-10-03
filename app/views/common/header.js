3ac67fc35b48e3a5ab234f8892eae046311d5195
absolux <a.mouhassine@gmail.com>
2015-10-03 20:30:40 +0100

    Add header view to the layout

---
+++ app/views/common/header.js
@@ -0,0 +1,38 @@
+/**
+ *  
+ */
+
+define(function(require, exports, module) {
+	'use strict';
+	
+	var Marionette = require('backbone.marionette');
+	var session = require('lib/session');
+	var router = require('router');
+	
+	var View = Marionette.ItemView.extend({
+		template: require('template!common/header'),
+		
+		events: {
+			'click .btn-logout': 'onLogoutClicked',
+		},
+		
+		serializeData: function() {
+			return {
+				isLoggedIn: session.has('user.token'),
+				username: session.get('user.name'),
+			};
+		},
+		
+		onBeforeShow: function() {
+			this.$el.attr('id', 'navigation');
+		},
+		
+		onLogoutClicked: function() {
+			// TODO should notify the api to invalidate the token
+			session.flush();
+			router.redirect('#/');
+		},
+	});
+	
+	module.exports = View;
+});
--- app/views/layout.js
+++ app/views/layout.js
@@ -8,17 +8,48 @@ define(function(require, exports, module) {
     var Marionette = require('backbone.marionette');
     
     var LayoutView = Marionette.LayoutView.extend({
-        el: false,
+        el: 'body',
         
         template: require('template!layout'),
         
         regions: {
             header: 'header',
             sidebar: 'aside',
-            content: 'main'
-        }
+            content: 'main',
+            footer: 'footer',
+        },
+        
+        /**
+         * Show the content view
+         * 
+         * @param {object} view
+         */
+        show: function(view) {
+            var that = this;
+            
+            if (! this.getRegion('header').hasView() ) {
+                require(['views/common/header'], function(Header) {
+                    that.getRegion('header').show(new Header());
+                });
+            }
+            
+            if (! this.getRegion('sidebar').hasView() ) {
+                /*require(['views/common/siddebar'], function(Sidebar) {
+                    that.getRegion('sidebar').show(new Sidebar());
+                });*/
+            }
+            
+            this.getRegion('content').show(view);
+        },
+        
+        /**
+         * Show a bootstrap modal in layout footer region
+         */
+        showModal: function(view) {
+            this.getRegion('footer').show(view);
+        },
     });
     
     // retrurn an instance of the layout as a singleton
-    module.exports = new LayoutView();
+    module.exports = (new LayoutView()).render();
 });
