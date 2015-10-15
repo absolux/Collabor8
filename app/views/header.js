d6771f9a604f6fd328a0d45e9bc1c86c5649c8ed
absolux <a.mouhassine@gmail.com>
2015-10-16 00:11:25 +0100

    adapt header view

---
+++ app/views/header.js
@@ -0,0 +1,39 @@
+/* 
+ * 
+ */
+
+define(function(require, exports, module) {
+    'use strict';
+    
+    var Backbone = require('backbone');
+    var session = require('lib/session');
+    
+    module.exports = Backbone.View.extend({
+        manage: true,
+        
+        className: 'container-fluid',
+        
+        template: require('template!header'),
+        
+        events: {
+			'click .btn-logout': 'onLogoutClicked',
+		},
+		
+		html: function($el, content) {
+            $el.html(content).delay(50).slideDown(100);
+        },
+        
+        serialize: function() {
+            return {
+                isLoggedIn: window.isLogged,
+				username: session.get('user.name'),
+            };
+        },
+		
+		onLogoutClicked: function() {
+			// TODO should notify the api to invalidate the current token
+			session.flush();
+			require('router').goLogin();
+		},
+    });
+});
