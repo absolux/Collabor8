33e1d3c3a0f48c38239687875c67cd5996e9205a
absolux <a.mouhassine@gmail.com>
2016-02-13 15:45:57 +0000

    adapt configuration files

---
+++ config.js
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+require.config({
+  
+  baseUrl: "app",
+  
+  paths: {
+    'jquery':               "../vendor/jquery/dist/jquery",
+    'toastr':               "../vendor/toastr/toastr",
+    'underscore':           "../vendor/underscore/underscore",
+    'backbone':             "../vendor/backbone/backbone",
+    'bootstrap':            "../vendor/bootstrap/dist/js/bootstrap",
+    'moment':               "../vendor/moment/moment",
+    'vue':                  "../vendor/vue/dist/vue",
+    'fullcalendar':         "../vendor/fullcalendar/dist/fullcalendar",
+    'text':                 "../vendor/requirejs-text/text",
+  },
+  
+  config: {},
+  
+  shim: {
+    'bootstrap': ['jquery']
+  },
+  
+  packages: ['common'],
+  
+  deps: ['main']
+  
+})
